5b93dcb1b60b7f0dcc5bdc675df18da79a761ecf
Daniel Munoz <lostlucidity@gmail.com>
2015-10-13 20:46:06 -0400

    Rails app to_do list first commit.

---
+++ app/assets/javascripts/todo_items.js
@@ -0,0 +1,10 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in applicati'on.js.
+
+
+$(document).on('ready', function(){
+	$(".check").on('click', function(){
+		console.log('it worked')
+	})
+	
+})
